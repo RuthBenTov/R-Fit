c5f609006685be1e39c7260585013805d03dddd8
danieltovbin <tovbindaniel@gmail.com>
2023-12-26 16:38:19 +0200

    addingPagesToRoutes
    Adding the other two pages:Logbook and Profile

--- client/src/routes/routes.tsx
+++ client/src/routes/routes.tsx
@@ -1,10 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom';
 import SignUp from '../pages/signUp/SignUp';
 import SignIn from '../pages/signIn/SignIn';
+import Logbook from '../pages/logbook/Logbook';
+import ProfilePage from '../pages/profile/ProfilePage';
 
 export const router = createBrowserRouter([
 
   { path: "/", element: <SignUp /> },
-  { path: "/add-user", element: <SignIn /> }
+  { path: "/add-user", element: <SignIn /> },
+  {path: "/logbook", element: <Logbook />},
+  {path: "/profile", element: <ProfilePage /> }
 
 ]);
