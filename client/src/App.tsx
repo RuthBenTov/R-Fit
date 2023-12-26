91342042d7eef8e10586b528a90cc6f01e6991b0
danieltovbin <tovbindaniel@gmail.com>
2023-12-26 13:47:35 +0200

    SignUpDesign&componentPassword

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,11 @@
 import './App.css'
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes/routes';
-// import SignIn from './pages/signIn/SignIn'
 
 function App() {
 
   return <RouterProvider router={router} />
-  // return (
-  //   <>
-  //    <SignIn/>
-   
-  //   </>
-  // )
+
 }
 
 export default App
--- client/src/pages/signUp/SignUp.tsx
+++ client/src/pages/signUp/SignUp.tsx
@@ -1,6 +1,10 @@
 import axios from 'axios';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import { Box, Container } from '@mui/material';
+import TextField from '@mui/material/TextField';
+import { InputPassword } from '../../components/passwordInput/Password';
 
 const SignUp = () => {
     const [userName, setUserName] = useState('');
@@ -8,33 +12,41 @@ const SignUp = () => {
     const navigate = useNavigate();
 
     const handleSignUp = async () => {
+
         try {
             if (!userName || !password) {
                 throw new Error("Please fill all fileds")
             }
             const response = await axios.post('http://localhost:3000/API/users/register', { userName, password })
-            console.log('response:',response.data);
-            navigate('/add-user')
+            console.log('response:', response.data);
+            handleLogin()
 
         } catch (error) {
             console.error(error)
         }
     }
 
+    const handleLogin = () => {
+        navigate('/add-user')
+    }
+
     return (
         <>
-            <h1>SignUp</h1>
-            <div>
-                <label>Username:</label>
-                <input type="text" value={userName} onChange={(ev) => setUserName(ev.target.value)} />
-            </div>
-            <div>
-                <label>Password:</label>
-                <input type="password" value={password} onChange={(ev) => setPassword(ev.target.value)} />
-            </div>
-            <button onClick={handleSignUp}>SignUp</button>
+            <Container sx={{ padding: '0px 4px 35px 3px', border: '1px solid gray', borderRadius: '12px' }}>
+                <h2 style={{ color: '#6467d4', marginBottom: '35px', fontSize: '25px' }}>SignUp</h2>
+                <Box sx={{ width: '100%' }}>
+                    <TextField variant='outlined' className="textField" fullWidth type="text" size="small" value={userName} placeholder="Username" onChange={(ev) => setUserName(ev.target.value)} />
+                    <InputPassword onInput={(ev) => setPassword(ev.target.value)} />
+                    <Button onClick={handleSignUp} size='small' variant='contained' type='submit' sx={{ backgroundColor: '#6467d4', color: 'white' }}>Signup</Button>
+                    <p style={{ paddingBottom: '150px', textAlign: 'center', color: 'gray' }}>Forget your password?</p>
+                    <div style={{ padding: '3px 2px', border: "1px solid gray", margin: 'auto', width: '1px', borderRadius: "12px", textAlign: 'center' }}></div>
+                    <br />
+                    <Button size='small' sx={{ color: '#6467d4' }} onClick={handleLogin} variant='outlined'>Login</Button>
+                </Box>
+            </Container>
         </>
     )
 }
 
 export default SignUp
+
