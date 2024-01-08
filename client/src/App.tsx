4af0bcc6572057f67666d70255f742288516d22e
danieltovbin <tovbindaniel@gmail.com>
2024-01-08 15:25:02 +0200

    profilePage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css'
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes/routes';
+import ProfilePage from './pages/profile/ProfilePage';
 
 function App() {
+  return(
+    <ProfilePage />
 
-  return <RouterProvider router={router} />
+  )
+
+  // return <RouterProvider router={router} />
 
 }
 
--- client/src/pages/profile/ProfilePage.tsx
+++ client/src/pages/profile/ProfilePage.tsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+import { Container } from '@mui/material';
+import CreateOutlinedIcon from '@mui/icons-material/CreateOutlined';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import CakeOutlinedIcon from '@mui/icons-material/CakeOutlined';
+import PhoneOutlinedIcon from '@mui/icons-material/PhoneOutlined';
 
 const ProfilePage = () => {
   return (
-    <div>ProfilePage</div>
+    <>
+      <Stack >
+        <Avatar sx={{ margin: "auto", padding: "40px" }} alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+      </Stack>
+      <h2>User name</h2>
+      <Container sx={{ padding: '0px 4px 35px 3px', border: '1px solid gray', borderRadius: '12px' }}>
+        <ListItemButton >
+          <ListItemText primary="Personal Details" />
+          <ListItemIcon sx={{ display: "flex", justifyContent: "space-evenly" }}>
+            <CreateOutlinedIcon />
+          </ListItemIcon>
+        </ListItemButton>
+
+        <ListItemButton>
+          <ListItemIcon>
+            <CakeOutlinedIcon />
+          </ListItemIcon>
+          <ListItemText primary="date" />
+        </ListItemButton>
+        
+        <ListItemButton>
+          <ListItemIcon>
+            <PhoneOutlinedIcon />
+          </ListItemIcon>
+          <ListItemText primary="phone" />
+        </ListItemButton>
+      </Container>
+    </>
   )
 }
 
