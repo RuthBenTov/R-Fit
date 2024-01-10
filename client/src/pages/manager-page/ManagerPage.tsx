1f544c54c93a4d610fe7cc0c548bb110a684173c
Ruth_BenTov_developer <112100490+RuthBenTov@users.noreply.github.com>
2024-01-10 23:12:16 +0200

    ruth

--- client/src/pages/manager-page/ManagerPage.tsx
+++ client/src/pages/manager-page/ManagerPage.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import FullCalendar from "@fullcalendar/react";
+import "bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-icons/font/bootstrap-icons.css"; // needs additional webpack config!
 // import { Calendar } from "@fullcalendar/core";
@@ -8,9 +9,10 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import listPlugin from "@fullcalendar/list";
 import { EventInput } from "@fullcalendar/core";
-
 import bootstrap5Plugin from "@fullcalendar/bootstrap5";
 import { duration } from "@mui/material";
+import { getThisDate } from "../../assets/functions";
+import "./managerPaseStyle.scss";
 
 interface TrainingTable {
   training_id: number;
@@ -24,29 +26,28 @@ interface TrainingTable {
 }
 
 const ManagerPage = () => {
+  const [isRecurClicked, setIsRecurClicked] = useState(false);
   const [events, setEvents] = useState<EventInput[]>([
     {
       title: "Ruth And Daniel", // a property!
       start: "2024-01-07T16:00:00", // a property!
       end: "2024-01-07T17:00:00", // a property! ** see important note below about 'end' **
-      timeStart: "20:00:00",
-      timeEnd: "20:00:00",
       recurring: true,
     },
     {
       title: "Ruth01", // a property!
       start: "2024-01-07T10:00:00", // a property!
       end: "2024-01-07T09:00:00", // a property! ** see important note below about 'end' **
-      timeStart: "15:00:00",
-      timeEnd: "17:00:00",
     },
     {
-      title: 'Meeting',
-      start: '2024-01-09T09:00:00', // תאריך ושעת התחלה
-      end: '2024-01-09T11:30:00', // תאריך ושעת סיום
-      duration: { hours: 2, minutes: 30 }, // משך הזמן של האירוע - 2 שעות ו-30 דקות
-      allDay: false, // אירוע לא כל היום
-    }
+      title: "Meeting",
+      start: "2024-01-09T09:00:00", // תאריך ושעת התחלה
+    },
+    {
+      title: "אירוע 45 דקות",
+      start: "2024-01-23T09:00:00",
+      duration: "00:45:00",
+    },
   ]);
 
   function addEvent(event: any) {
@@ -56,38 +57,37 @@ const ManagerPage = () => {
       trainer: event.target.trainer.value,
       date: event.target.date.value,
       timeStart: event.target.timeStart.value,
-      duration: parseInt(event.target.duration.value),
-      isRegular: event.target.isRegular.checked,
+      duration: parseInt(event.target.duration?.value),
+      isRecurring: event.target.isRecurring.checked,
     };
-    console.log(formData.timeStart)
-    const newEvent: EventInput = formData.isRegular?{
-      title: `${formData.trainingName}`,
-      startTime: `${formData.date}T${formData.timeStart}`,
-      end: `${formData.date}T${formData.timeStart}`,
-      duration: { hours: 0, minutes: formData.duration },
-      allDay: false,
-    }:{
-      title: `${formData.trainingName}`,
-      startTime: `${formData.date}T${formData.timeStart}`,
-      end: `${formData.date}T${formData.timeStart}`,
-      duration: { hours: 0, minutes: formData.duration },
-      allDay: false,
-      daysOfWeek: [new Date(formData.date).getDay()],
-      recurring: formData.isRegular,
-      rrule: {
-        freq: "weekly",
-      },
-    }
 
+    const newEvent: EventInput = !isRecurClicked
+      ? {
+          title: `${formData.trainingName}`,
+          start: `${formData.date}T${formData.timeStart}:00`,
+          // end: `${formData.date}T${formData.timeStart}`,
+          duration: { minutes: formData.duration },
+          // allDay: false,
+        }
+      : {
+          title: `${formData.trainingName}`,
+          start: `${formData.date}T${formData.timeStart}:00`,
+          startTime: `${formData.timeStart}:00`,
+          endTime: `${formData.timeStart}:00`,
+          duration: { minutes: formData.duration },
+          allDay: false,
+          daysOfWeek: [new Date(formData.date).getDay()],
+          recurring: formData.isRecurring,
+        };
+    console.log(newEvent.start);
     console.log(events);
 
     setEvents((prev) => [...prev, newEvent]);
   }
-
   return (
-    <div>
-      <h2>ManagerPage</h2>
-      <form onSubmit={(ev) => addEvent(ev)}>
+    <div className="ManagerPageDiv">
+      <form className="addEventForm box" onSubmit={(ev) => addEvent(ev)}>
+      <h2>Manager Page</h2>
         <input
           type="text"
           name="trainingName"
@@ -97,38 +97,52 @@ const ManagerPage = () => {
         <input type="text" name="trainer" id="trainer" placeholder="Trainer" />
         <input type="date" name="date" id="date" />
         <input type="time" name="timeStart" id="timeStart" />
+        <label htmlFor="isRecurring">is recurring?</label>
         <input
-          type="number"
-          name="duration"
-          id="duration"
-          placeholder="duration"
-        />
-        <label htmlFor="isRegular">is regular?</label>
-        <input type="checkbox" name="isRegular" id="isRegular"></input>
+          type="checkbox"
+          name="isRecurring"
+          id="isRecurring"
+          onChange={() => {
+            setIsRecurClicked(!isRecurClicked);
+          }}
+        ></input>
+        {isRecurClicked ? (
+          <input
+            type="number"
+            name="duration"
+            id="duration"
+            placeholder="min duration"
+          />
+        ) : (
+          <></>
+        )}
         <button type="submit">add event</button>
       </form>
-      <FullCalendar
-        plugins={[
-          dayGridPlugin,
-          interactionPlugin,
-          timeGridPlugin,
-          listPlugin,
-          bootstrap5Plugin,
-        ]}
-        themeSystem="bootstrap5"
-        headerToolbar={{
-          left: "prev,next today",
-          center: "title",
-          right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek",
-        }}
-        initialDate="2018-01-12"
-        navLinks={true} // can click day/week names to navigate views
-        editable={true}
-        dayMaxEvents={true} // allow "more" link when too many events
-        dayHeaderFormat={{ weekday: "short" }}
-        initialView="dayGridWeek"
-        events={events}
-      />
+      <div className="calendar">
+        <FullCalendar
+          plugins={[
+            dayGridPlugin,
+            interactionPlugin,
+            timeGridPlugin,
+            listPlugin,
+            bootstrap5Plugin,
+          ]}
+          themeSystem="bootstrap5"
+          headerToolbar={{
+            left: "prev,next today",
+            center: "title",
+            right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek",
+          }}
+          initialDate={getThisDate()}
+          navLinks={true} // can click day/week names to navigate views
+          editable={true}
+          dayHeaderFormat={{ weekday: "short" }}
+          initialView="dayGridWeek"
+          height="400px"
+          aspectRatio={2}
+          events={events}
+        />
+      </div>
     </div>
   );
 };
