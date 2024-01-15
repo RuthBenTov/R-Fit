bf536a2c414699daba93c58ab5caacbcb3a2e995
Ruth_BenTov_developer <112100490+RuthBenTov@users.noreply.github.com>
2024-01-15 15:00:17 +0200

    ruth

--- client/src/API/eventCtrl.ts
+++ client/src/API/eventCtrl.ts
@@ -1,11 +1,10 @@
 import axios  from 'axios';
 export async function addEventDb(eventDb: { trainingName: any; trainer: any; date: any; timeStart: any; duration: number; isRecurring: any; }) {
   try {
+    console.log(eventDb)
     const {data} = await axios.post("/API/events/addEvent", {eventDb})
-    console.log(data.status) 
-    if(data.status == "ok"){
-        return data.status
-    }
+    console.log(data.ok) 
+    if(data) return data
     else throw new Error ("Could not add event to DB")
   } catch (error) {
     console.error(error);
--- server/API/Events/eventCtrl.ts
+++ server/API/Events/eventCtrl.ts
@@ -4,30 +4,33 @@ export async function addEvent(req, res) {
   try {
     const { eventDb } = req.body;
     console.log(eventDb);
-   
-    // const query = `INSERT INTO r_fit.training ("training_name", "date_time", "day", "is_regular", "name_trainer", "program_training_id", "duration")
-    // VALUES ("${eventDb.trainingName}", "${eventDb.date} ${eventDb.timeStart}:00", ${new Date(eventDb.date).getDay().toString()}, ${eventDb.isRecurring ? 1 : 0}, "${eventDb.trainer}", 0, "${eventDb.duration}")`;
+    const query = `INSERT INTO r_fit.training (training_name, date_time, day, is_regular, name_trainer, program_training_id, duration)  VALUES ("${
+      eventDb.trainingName
+    }", "${eventDb.date} ${eventDb.timeStart}:00", ${new Date(eventDb.date)
+      .getDay()
+      .toString()}, ${eventDb.isRecurring ? 1 : 0}, "${eventDb.trainer}", 0, "${
+      eventDb.duration || 60
+    }")`;
 
-   const query = `INSERT INTO r_fit.training (training_name, date_time, day, is_regular, name_trainer, program_training_id, duration)  VALUES ("${eventDb.trainingName}", "${eventDb.date} ${eventDb.timeStart}:00", ${new Date(eventDb.date).getDay().toString()}, ${eventDb.isRecurring ? 1 : 0}, "${eventDb.trainer}", 0, "${eventDb.duration}")`;
-
-    connection.query(query, (err, result)=>{
+    connection.query(query, (err, result) => {
       try {
         if (err) throw err;
         //@ts-ignore
-        if (result.changedRows){
-          res.send({ok:true , result})
+        console.log(result.changedRows);
+        //@ts-ignore
+        if (result.changedRows) {
+        res.send({ ok: true, result });
+        } else {
+        res.send({ ok: false, result: "no changes found" });
         }
-        else {
-          res.send({ok:false , result:"no changes found"}) }
       } catch (error) {
         console.log(error);
-        res.status(500).send({ok:false, error})
+        res.status(500).send({ ok: false, error });
       }
-    })
+    });
   } catch (error) {
     console.error(error);
-    res.status(500).send({ok:false, error})
-
+    res.status(500).send({ ok: false, error });
   }
 }
 
