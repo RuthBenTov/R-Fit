1a9467ab9929832bb4a87f1643f922111b6d30f4
danieltovbin <tovbindaniel@gmail.com>
2024-02-02 18:51:47 +0200

    .

--- server/config/allowedOrigin.ts
+++ server/config/allowedOrigin.ts
@@ -4,4 +4,4 @@ dotenv.config();
 
 const urlPROD = process.env.CLIENT_PROD_URL 
 const urlDEV = process.env.CLIENT_DEV_URL 
-export const allowedOrigins = [urlPROD,urlDEV];
+export const allowedOrigins = [urlDEV,urlPROD];
